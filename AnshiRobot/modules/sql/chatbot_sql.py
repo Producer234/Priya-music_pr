420cae81415fb3e1ac1a214fbed6cf381854ec93
Producer234 <prexplanation2@gmail.com>
2026-02-12 00:53:03 +0600

    Update chatbot_sql.py

--- AnshiRobot/modules/sql/chatbot_sql.py
+++ AnshiRobot/modules/sql/chatbot_sql.py
@@ -1,43 +1,16 @@
 import threading
+from AnshiRobot.modules.sql import BASE
 
-from sqlalchemy import Column, String
-
-from AnshiRobot.modules.sql import BASE, SESSION
-
-
-class AnshiChats(BASE):
-    __tablename__ = "Anshi_chats"
-    chat_id = Column(String(14), primary_key=True)
-
-    def __init__(self, chat_id):
-        self.chat_id = chat_id
-
-
-AnshiChats.__table__.create(checkfirst=True)
-INSERTION_LOCK = threading.RLock()
-
+CHATBOT = BASE["chatbot"]
+LOCK = threading.RLock()
 
 def is_Anshi(chat_id):
-    try:
-        chat = SESSION.query(AnshiChats).get(str(chat_id))
-        return bool(chat)
-    finally:
-        SESSION.close()
-
+    return CHATBOT.find_one({"chat_id": str(chat_id)}) is not None
 
 def set_Anshi(chat_id):
-    with INSERTION_LOCK:
-        Anshichat = SESSION.query(AnshiChats).get(str(chat_id))
-        if not Anshichat:
-            Anshichat = AnshiChats(str(chat_id))
-        SESSION.add(Anshichat)
-        SESSION.commit()
-
+    with LOCK:
+        CHATBOT.insert_one({"chat_id": str(chat_id)})
 
 def rem_Anshi(chat_id):
-    with INSERTION_LOCK:
-        Anshichat = SESSION.query(AnshiChats).get(str(chat_id))
-        if Anshichat:
-            SESSION.delete(Anshichat)
-        SESSION.commit()
-      
+    with LOCK:
+        CHATBOT.delete_one({"chat_id": str(chat_id)})
